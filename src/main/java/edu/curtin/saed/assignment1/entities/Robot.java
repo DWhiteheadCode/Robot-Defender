9ef049563a2742a4170d1c1c806e38c8208ee692
DWhiteheadCode <95430831+DWhiteheadCode@users.noreply.github.com>
2023-09-05 16:10:57 +0800

    Implement *basic* Robot functionality. Moving unfinished

--- src/main/java/edu/curtin/saed/assignment1/entities/Robot.java
+++ src/main/java/edu/curtin/saed/assignment1/entities/Robot.java
@@ -1,7 +1,77 @@
 package edu.curtin.saed.assignment1.entities;
 
-public class Robot 
+import edu.curtin.saed.assignment1.GameEngine;
+
+public class Robot implements Runnable
 {
+    private final int id;
+    private final long moveDelayMilliseconds;
+    private GameEngine gameEngine;
+    private Coordinates coordinates;
+    
+    public Robot(int id, long moveDelayMilliseconds)
+    {
+        this.id = id;
+        this.moveDelayMilliseconds = moveDelayMilliseconds;
+        this.gameEngine = null;
+        this.coordinates = null;
+    }
+
+    public void setGameEngine(GameEngine engine)
+    {
+        this.gameEngine = engine;
+    }
+
+    public void setCoordinates(Coordinates coordinates)
+    {
+        this.coordinates = coordinates;
+    }
+
+    @Override
+    public void run() 
+    {
+        if(this.gameEngine == null)
+        {
+            throw new IllegalStateException("Can't start robot before setting its game engine.");
+        }
+
+        if(this.coordinates == null)
+        {
+            throw new IllegalStateException("Can't start robot before setting its coordinates.");
+        }
+
+        try
+        {
+            boolean dead = false;
+            do
+            {
+                //Comapre coords with citadel coords
+
+                //Sort possible moves based on ordered-randomness
+
+                //Attempt to make moves until one succeeds
+                
+                Thread.sleep(moveDelayMilliseconds);
+            }
+            while(!dead);
+        }
+        catch(InterruptedException iE)
+        {
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
