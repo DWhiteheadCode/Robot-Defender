41118ddaccfeaef8e89ebd4f4eacbefb98c6a1a1
DWhiteheadCode <95430831+DWhiteheadCode@users.noreply.github.com>
2023-09-05 18:45:17 +0800

    Basic FortressWall implementation

--- src/main/java/edu/curtin/saed/assignment1/entities/FortressWall.java
+++ src/main/java/edu/curtin/saed/assignment1/entities/FortressWall.java
@@ -1,6 +1,35 @@
 package edu.curtin.saed.assignment1.entities;
 
+import edu.curtin.saed.assignment1.GameEngine;
+import edu.curtin.saed.assignment1.misc.Coordinates;
+
 public class FortressWall 
 {
-    
+    private Coordinates coordinates;
+    private boolean isDamaged;
+
+    private GameEngine gameEngine;
+
+    public FortressWall(GameEngine gameEngine)
+    {
+        this.gameEngine = gameEngine;
+        this.isDamaged = false;
+        this.coordinates = null;
+    }
+
+    public void setCoordinates(Coordinates coordiantes)
+    {
+        this.coordinates = coordiantes;
+    }
+
+    public Coordinates getCoordinates()
+    {
+        return new Coordinates(this.coordinates);
+    }
+
+    public boolean isDamaged()
+    {
+        return isDamaged;
+    }
+
 }
